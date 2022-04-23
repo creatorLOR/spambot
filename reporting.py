ebbe143d215a0cb1b7593aca2cb8028c9908eaa1
creatorLOR <104263867+creatorLOR@users.noreply.github.com>
2022-04-23 18:30:18 +0430

    Update and rename README.md to reporting.py

---
+++ reporting.py
@@ -0,0 +1,440 @@
+# spam - bot
+
+# friend: ali , mmd
+
+# python mmd ryson , ali lor
+#__
+import os
+import time
+#___
+os.system("clear")
+
+time.sleep(0.5)
+
+print()
+yea = input("dadash! ♥️ ketabkhuneh haro barat nasb konam ya na? \n\n (y/n) >>>> ")
+print()
+print()
+if yea == "y".lower():
+    print()
+    os.system("pip install requests")
+    os.system("pip install colored")
+    os.system("pip install colorama")
+    os.system("pip install datetime")
+    os.system("pip install flags")
+    print()
+    print("haleh dadash")
+    #
+#
+
+if yea == "n".lower():
+    print()
+# importings __
+import string
+import sys , requests, colorama, colored
+
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style
+
+import random
+import datetime
+
+#___
+
+os.system("clear")
+print()
+time.sleep(0.5)
+print ()
+os.system("clear")
+time.sleep(1)
+print (Fore.YELLOW + '')
+print ("\n"*2)
+#bnr
+banner = ["""
+   
+    \   ^__^
+     \  (oo)\_______
+        (__)\       )\/\
+            ||----w |
+            ||     ||
+
+                
+ ___  _ __    __ _  _ __ ___  
+/ __|| '_ \  / _` || '_ ` _ \ 
+\__ \| |_) || (_| || | | | | |
+|___/| .__/  \__,_||_| |_| |_|
+     |_|                      
+     
+     v-1                                            
+""","""
+               ______         _____ 
+______________ ___  /_ ______ __  /_
+__  ___/_  __ \__  __ \_  __ \_  __/
+_  /    / /_/ /_  /_/ // /_/ // /_  
+/_/     \____/ /_.___/ \____/ \__/  
+                                    
+
+"""]
+
+bnr = (random.choice(banner))
+
+print ()
+
+for m in bnr:
+    sys.stdout.write(m)
+    sys.stdout.flush()
+    time.sleep(0.01)
+
+timer = str(datetime.datetime.now())
+
+# 
+
+#felan
+
+def random_string_generator(str_size, allowed_chars):
+  return ''.join(random.choice(allowed_chars) for x in range(str_size))
+chars = string.ascii_letters + string.punctuation
+size = 6
+
+cod = (random_string_generator(size, chars))
+
+#
+print ()
+time.sleep(1)
+print ()
+print (Fore.GREEN + '')
+
+spam = f"""
+       ⟩ LOR SCRIPT ⟨
+    
+            
+           ┬─┐┌─┐┌─┐┌─┐┬─┐┌┬┐┌─┐┬─┐
+           ├┬┘├┤ ├─┘│ │├┬┘ │ ├┤ ├┬┘
+           ┴└─└─┘┴  └─┘┴└─ ┴ └─┘┴└─
+
+
+| time: {timer} |
+
+
+type >'acc'< spam rubika @supportbot
+
+type >'channel'< spam rubika @supportbot
+
+type >'group'< spam rubika @supportbot
+
+
+"""
+for mmd in spam:
+    sys.stdout.write(mmd)
+    sys.stdout.flush()
+    time.sleep(0.01)
+#
+print(Fore.YELLOW + '')
+spammer = input("please enter your type spam ⟩⟩ ")
+
+print ()
+print ()
+print (Fore.BLUE +"")
+print("\rscanning..",end="",flush=False)
+time.sleep(1)
+print("\rscanning....",end="",flush=False)
+time.sleep(1)
+print("\rscanning.....",end="",flush=False)
+time.sleep(1)
+print("\rscanning......",end="",flush=False)
+time.sleep(1)
+print("\rscanning.......",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning........",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning.........",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning..........",end="",flush=False)
+time.sleep(1)
+print("\r scanned          ",end="",flush=False)
+time.sleep(1)
+print (Fore.WHITE + '')
+print()
+print()
+print("<>")
+time.sleep(0.5)
+print()
+print(Fore.BLUE + '')
+username = input("please enter username target [@username] ⟩⟩ ")
+
+time.sleep(1)
+
+print ()
+# spcial !
+
+x = (f"""spam for username [{username}] please transfer >> rubika.ir/supportbot""")
+#_
+ 
+# account spam
+
+rand = [f"""
+
+[[ please check username This user has attempted to broadcast illegal content and spam: ({username}) \n \ntime: ({timer}) \n \n ({cod}) ]]""",
+
+f"""
+[[ Content that is illegal or illegal Rubikas please check: ({username}) \n \n time: ({timer}) \n \n ({cod}) ]]""",
+
+f"""
+[[ This user has attempted to broadcast content that is against Rubika rules with this account. Please follow up as soon as possible: {username} \n \n time: ({timer}) \n \n({cod}) ]""",f"""
+This person is threatening to hack and acts against the rules of Rubika and spam, please report: ({username})
+
+time: ({timer})
+
+({cod}) ]]""",F"""
+Hello dear admin, thank you for checking this file. Please see the violations of this user. Thank you: ({username})
+
+time: ({timer})
+
+({cod}) ]]""",F"""
+[[ سلام ادمین گرامی از اینکه این پرونده را مشاهده میکنید بسیار خرسندم، لطفاًبه تخلفات بی انتهای این کاربر متخلف و هرزنامه رسیدگی کنید باتشکر: ({username})
+
+time: ({timer})
+
+({cod}) ]]"""]
+
+rand1 = (random.choice(rand))
+
+print (Fore.YELLOW + '')
+# channel spam
+rando = [f"""
+
+[[ Please report this channel
+that has illegal content and spam !: 
+({username}) \n\n time: ({timer}) \n \n ({cod}) ]]""",
+
+F"""
+[[ This channel broadcasts obscene,pornographic and inappropriate content: 
+({username}) \n\n time: ({timer}) \n \n ({cod}) ]]
+
+""",f"""[[ \n این کانال دارای محتوایات زشت و پورنوگرافی و کودک آزاری و نامناسبی است لطفاً پیگیری کنید : ({username}) \n\ntime: ({timer})\n\n({cod}) ]]""",f"""[[ This channel is broadcasting illegal content, please report as soon as possible !!!!!: ({username})\n\ntime: ({timer})\n \n({cod}) ]]"""]
+
+rando1 = (random.choice(rando))
+
+# group spam
+
+
+randi = [f"""[[ Please check that this group uses annoying, inappropriate and insulting texts in this group and needs your help and reporting !!! :\n {username} \ntime: ({timer})\n\n({cod}) ]]""",f"""[[ لطفاً این گروه را که محتوای خلاف قوانین دارد و در آن فحاشی میشود چک کنید :
+ {username} \ntime: ({timer}) \n\n({cod})  ]]""",f"""[[ Misuse and obscenity Please Report this Rubika group where they post inappropriate chats and child abuse !!! : \n {username} \ntime: ({timer}) \n\n({cod}) ]]""",f"""
+ Check illegal content This user has started playing spam content! : ({username})
+ 
+time: ({timer})
+
+({cod}) ]]"""]
+
+randi1 = (random.choice(randi))
+
+# end ok
+
+# if
+
+if spammer == "acc":
+    ##
+    print ()
+    print(Fore.BLUE + '')
+    time.sleep(1)
+    print("spam support bot ↓↓↓↓ :")
+    time.sleep(0.5)
+    print()
+    print()
+    print(Fore.GREEN + '')
+    print("________________________")
+    time.sleep(1)
+    print (Fore.YELLOW + '')
+    print (rand1)
+    print ()
+    print(Fore.GREEN + '')
+    time.sleep(0.5)
+    print("________________________")
+    print (Fore.RED + '')
+    time.sleep(1)
+    print (x)
+    print ()
+
+if spammer == "channel".lower():
+    time.sleep(1)
+    print()
+    print(Fore.GREEN + '')
+    user = input("url target check? (y/n) ⟩⟩⟩ ")
+    if user == "y".lower():
+        time.sleep(0.5)
+        print(Fore.BLUE + '')
+        print()
+        usr = input("enter url target rubika don't '@'  ⟩⟩⟩ ")
+        print()
+        req = requests.get(f"https://rubika.ir/{usr}")
+        if req.status_code == 200:
+            print()
+            print(Fore.YELLOW + '')
+            print ("channel found *_*")
+            time.sleep(1)
+            #
+
+        elif req.status_code == 404:
+            print()
+            print(Fore.RED + '')
+            print ("not found channel *_*")
+            print()
+            time.sleep(1)
+            #
+        
+
+        elif req.status_code == 302:
+            print()
+            print(Fore.RED + '')
+            print("not found channel *_*")
+            print()
+            time.sleep(1)
+            #
+        
+        else:
+            pass
+    if user == "n":
+        print()
+        time.sleep(0.5)
+        print()
+    time.sleep(1)
+    print()
+    print ()
+    print(Fore.BLUE + '')
+    print("spam support bot ↓↓↓↓ :")
+    print()
+    time.sleep(1)
+    print(Fore.GREEN + '')
+    print("________________________")
+    time.sleep(0.5)
+    print (Fore.YELLOW + '')
+    print (rando1)
+    print ()
+    time.sleep(0.5)
+    print(Fore.GREEN + '')
+    print("________________________")
+    print (Fore.RED + '')
+    time.sleep(1)
+    print (x)
+    print ()
+
+    
+if spammer == "group".lower():
+    time.sleep(1)
+    print()
+    print(Fore.GREEN + '')
+    user1 = input("url target check? (y/n) ⟩⟩⟩ ")
+    if user == "y".lower():
+        time.sleep(0.5)
+        print(Fore.BLUE + '')
+        print()
+        usr1 = input("enter url target rubika type group  ⟩⟩⟩ ")
+        print()
+        reqs = requests.get(f"{usr1}")
+        if reqs.status_code == 200:
+            print()
+            print(Fore.YELLOW + '')
+            print ("group found *_*")
+            time.sleep(1)
+            #
+
+        elif reqs.status_code == 404:
+            print()
+            print(Fore.RED + '')
+            print ("not found group *_*")
+            print()
+            time.sleep(1)
+            #
+        
+
+        elif reqs.status_code == 302:
+            print()
+            print(Fore.RED + '')
+            print("not found group *_*")
+            print()
+            time.sleep(1)
+            #
+        
+        else:
+            pass
+    time.sleep(2)
+    print()
+    print ()
+    print(Fore.BLUE + '')
+    print("spam support bot ↓↓↓ : ")
+    print ()
+    print()
+    time.sleep(1)
+    print(Fore.GREEN + '')
+    print("_______________________")
+    time.sleep(0.5)
+    print (Fore.YELLOW + '')
+    print (randi1)
+    print ()
+    time.sleep(0.5)
+    print(Fore.GREEN + '')
+    print("_______________________")
+    print (Fore.RED + '')
+    time.sleep(1)
+    print (x)
+    print ()
+
+##
+time.sleep(1)
+input()
+# if end    
+print ()
+print (Fore.BLUE + '')
+
+baner = f"""
+
+------------------
+target: {username}
+
+time spam: {timer}
+------------------
+
+
+###### # #      ##### ###### #####  # #    #  ####
+ #      # #        #   #      #    # # ##   # #    #
+ #####  # #        #   #####  #    # # # #  # #
+ #      # #        #   #      #####  # #  # # #  ###
+ #      # #        #   #      #   #  # #   ## #    #
+ #      # ######   #   ###### #    # # #    #  ####
+
+
+-----------------------------------
+info: [ali-lor.blogfa.com] -ali lor
+-----------------------------------
+----------------------------------
+info: [creater.blog.ir] -mmd ryson
+----------------------------------
+
+"""
+for md in baner:
+    sys.stdout.write(md)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+print ()
+print (Fore.RED + '')
+time.sleep(3)
+print()
+
+exit = input ("restart-(1) || exit-(2) ⟩⟩ ")
+
+if exit == 1:
+    print ()
+    time.sleep(1)
+    os.system("python reporting.py")
+    print ()
+
+
+if exit == 2:
+    print ()
+    time.sleep(1)
+    sys.exit()
+    os.system("exit 0")
+    
+sys.exit()
+# the end :))    
